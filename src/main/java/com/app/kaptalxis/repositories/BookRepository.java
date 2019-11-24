dfe22e970ac43c5b79b9f9a9c0d08e3e8404515c
Aleksandr Pimenov <aleksandrinstall@gmail.com>
2019-11-24 16:36:21 +0300

    add file uploading

--- src/main/java/com/app/kaptalxis/repositories/BookRepository.java
+++ src/main/java/com/app/kaptalxis/repositories/BookRepository.java
@@ -1,6 +1,8 @@
 package com.app.kaptalxis.repositories;
 
 import com.app.kaptalxis.models.Book;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -11,4 +13,5 @@ import java.util.UUID;
 public interface BookRepository extends JpaRepository<Book, UUID> {
     List<Book> findByTitleIgnoreCase(String title);
     Book findByAuthorIgnoreCase(String author);
+    Page<Book> findAll(Pageable pageable);
 }
--- src/main/java/com/app/kaptalxis/services/BookServiceImplementation.java
+++ src/main/java/com/app/kaptalxis/services/BookServiceImplementation.java
@@ -3,15 +3,29 @@ package com.app.kaptalxis.services;
 import com.app.kaptalxis.models.Book;
 import com.app.kaptalxis.repositories.BookRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
 
+import java.io.File;
+import java.io.IOException;
 import java.util.List;
+import java.util.Objects;
+import java.util.UUID;
 
 @Service
-public class BookServiceImplementation implements BookService{
+public class BookServiceImplementation implements BookService {
 
     @Autowired
     private BookRepository bookRepository;
+
+    @Value("${upload.path.img}")
+    private String uploadPathImg;
+
+    @Value("${upload.path.file}")
+    private String uploadPathFile;
 
     @Override
     public List<Book> findBooksByTitle(String title) {
@@ -19,31 +33,95 @@ public class BookServiceImplementation implements BookService{
     }
 
     @Override
-    public boolean createBook(Book book) {
-        Book bookFromDb = bookRepository.findByAuthorIgnoreCase(book.getAuthor());
-        if(bookFromDb != null) return false;
-        bookRepository.save(book);
-        return true;
+    public List<Book> getAllBooks() {
+        return bookRepository.findAll();
     }
 
     @Override
-    public boolean updateBook(Book book) {
-        Book bookFromDb = bookRepository.findByAuthorIgnoreCase(book.getAuthor());
-        if(bookFromDb != null) return false;
-        bookRepository.save(book);
-        return true;
+    public boolean markRead(Book book, boolean mark) {
+        if (isBookInDb(book)) {
+            book.setReadAlready(mark);
+            return true;
+        } else return false;
     }
+
+    @Override
+    public boolean createBook(String title, String description,
+                              String author, String isbn, String printYear,
+                              MultipartFile file, MultipartFile img
+    ) throws IOException {
+        if (Objects.isNull(bookRepository.findByTitleIgnoreCase(author))) {
+            Book book = new Book();
+            book.setTitle(title);
+            book.setDescription(description);
+            book.setAuthor(author);
+            book.setIsbn(isbn);
+            book.setPrintYear(Integer.parseInt(printYear));
+            book.setReadAlready(false);
+            saveBookFile(book,file);
+            saveBookFile(book,img);
+            return true;
+        } else return false;
+    }
+
+    @Override
+    public boolean updateBook(Book book, String title,
+                              String description, String isbn,
+                              String printYear
+    ) {
+        if (isBookInDb(book)) {
+            book.setTitle(title);
+            book.setDescription(description);
+            book.setIsbn(isbn);
+            book.setPrintYear(Integer.parseInt(printYear));
+            book.setReadAlready(false);
+            return true;
+        } else return false;
+    }
+
 
     @Override
     public boolean deleteBook(Book book) {
-        Book bookFromDb = bookRepository.findByAuthorIgnoreCase(book.getAuthor());
-        if(bookFromDb != null) return false;
+        if (isBookInDb(book)) return false;
         bookRepository.delete(book);
         return true;
     }
 
     @Override
-    public List<Book> getAllBooks() {
-        return bookRepository.findAll();
+    public boolean saveBookFile(Book book, MultipartFile file) throws IOException {
+        if (file != null && !file.getOriginalFilename().isEmpty()) {
+            book.setFilePath(saveFile(file, uploadPathFile));
+            return true;
+        } else return false;
+    }
+
+    @Override
+    public boolean saveBookImg(Book book, MultipartFile img) throws IOException {
+        if (img != null && !img.getOriginalFilename().isEmpty()) {
+            book.setImgPath(saveFile(img, uploadPathImg));
+            return true;
+        } else return false;
+    }
+
+    @Override
+    public Page givePages(Integer size, Integer page) {
+        if (size != null | page != null)
+            return bookRepository.findAll(PageRequest.of(page, size));
+        return null;
+    }
+
+    private String saveFile(MultipartFile fileOrImg, String uploadPath) throws IOException {
+        File uploadDir = new File(uploadPath);
+        if (uploadDir.exists()) uploadDir.mkdir();
+        String uuidFile = UUID.randomUUID().toString();
+        String resultFileName = uuidFile + "." + fileOrImg.getOriginalFilename();
+
+        fileOrImg.transferTo(new File(uploadPath + "/" + resultFileName));
+        return resultFileName;
+    }
+
+    private boolean isBookInDb(Book book) {
+        Book bookFromDb = bookRepository.findByAuthorIgnoreCase(book.getAuthor());
+        return bookFromDb != null;
     }
 }
