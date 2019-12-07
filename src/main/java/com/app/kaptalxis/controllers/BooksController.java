61be21444486667c4cac5faedf7e0c866a8de1e6
Aleksandr Pimenov <aleksandrinstall@gmail.com>
2019-12-07 22:38:13 +0300

    Add booService tests

--- src/main/java/com/app/kaptalxis/controllers/BooksController.java
+++ src/main/java/com/app/kaptalxis/controllers/BooksController.java
@@ -5,8 +5,6 @@ import com.app.kaptalxis.services.BookService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
-import org.springframework.data.domain.Pageable;
-import org.springframework.data.web.PageableDefault;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
--- src/main/java/com/app/kaptalxis/repositories/BookRepository.java
+++ src/main/java/com/app/kaptalxis/repositories/BookRepository.java
@@ -6,17 +6,13 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import java.util.List;
 import java.util.UUID;
 
 @Repository
 public interface BookRepository extends JpaRepository<Book, UUID> {
     Book findByTitleIgnoreCase(String title);
+
     Page<Book> findAll(Pageable pageable);
+
     Page<Book> findByDescriptionContaining(String description, Pageable pageable);
-
-
-    List<Book> findByDescriptionIgnoreCase(String description);
-    Page<Book> findByDescriptionIgnoreCase(String description, Pageable pageable);
-    Page<Book> findByDescriptionLike(String description, Pageable pageable);
 }
