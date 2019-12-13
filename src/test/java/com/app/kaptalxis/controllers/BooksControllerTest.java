cd84766a9706338f0a3b8f471ab0797cf182ff29
Aleksandr Pimenov <aleksandrinstall@gmail.com>
2019-12-13 22:30:34 +0300

    Add bookController tests(3)

--- src/test/java/com/app/kaptalxis/controllers/BooksControllerTest.java
+++ src/test/java/com/app/kaptalxis/controllers/BooksControllerTest.java
@@ -1,6 +1,5 @@
 package com.app.kaptalxis.controllers;
 
-import com.app.kaptalxis.models.Book;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -11,7 +10,6 @@ import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.context.jdbc.Sql;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.request.MockHttpServletRequestBuilder;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
@@ -41,20 +39,36 @@ public class BooksControllerTest {
 
     @Test
     public void createBookTest() throws Exception {
-//        MockHttpServletRequestBuilder multipart = multipart("/book")
-//                .param("id","431c8960-047f-4d15-bccf-1397d9f00d05")
-//                .param("title","testTitleThree")
-//                .param("author","testAuthor")
-//                .param("description","testDescriptionThree")
-//                .param("isbn","testISBN")
-//                .param("print_year","1113")
-//                .param("read_already","false")
-//                .param("img_path","test/img/path")
-//                .param("file_path", "test/file/path");
-        this.mockMvc.perform(post("/book").)
+        this.mockMvc.perform(post("/book").contentType(MediaType.APPLICATION_JSON)
+                .content("{\"id\": null,\"title\": \"moreTitle\", \"description\": \"moreDescription\"," +
+                        "\"author\": \"moreAuthor\", \"isbn\": \"moreISBN\", \"printYear\": 2019, " +
+                        "\"readAlready\": false, \"imgPath\": null, \"filePath\": null }"))
                 .andExpect(status().isOk())
                 .andExpect(content().contentType(MediaType.APPLICATION_JSON_UTF8))
                 .andDo(print());
     }
 
+    @Test
+    public void markReadTest() throws Exception {
+        this.mockMvc.perform(patch("/book/mark/411c8960-047f-4d15-bccf-1397d9f00d05"))
+                .andExpect(status().isOk())
+                .andExpect(content().contentType(MediaType.APPLICATION_JSON_UTF8))
+                .andDo(print());
+    }
+
+    @Test
+    public void findByPhrase() throws Exception {
+        this.mockMvc.perform(get("/book/findByPhrase/test"))
+                .andExpect(status().isOk())
+                .andExpect(content().contentType(MediaType.APPLICATION_JSON_UTF8))
+                .andDo(print());
+    }
+
+    @Test
+    public void findBookByIdTest() throws Exception {
+        this.mockMvc.perform(get("/book/findById/421c8960-047f-4d15-bccf-1397d9f00d05"))
+                .andExpect(status().isOk())
+                .andExpect(content().contentType(MediaType.APPLICATION_JSON_UTF8))
+                .andDo(print());
+    }
 }
